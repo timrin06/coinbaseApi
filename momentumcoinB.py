73697164fdadd20c03e320f831d55780e3e987c5
Tim Tim <timrin20@gmail.com>
2021-11-08 23:36:58 +0300

    ok

--- momentumcoinB.py
+++ momentumcoinB.py
@@ -1,19 +1,12 @@
 import websockets
-import asyncio
 import json
-import time
 import datetime
+import time
 import numpy as np
 from coinbase.wallet.client import Client
 
 client = Client('Su2KNCgW9bZIw9Pp', 'XxTyWtwPou05c52MWc2L7sdu9GRoIlLG', api_version='YYYY-MM-DD')
 currency_code = 'USD'
-
-xdata = []
-ydata = []
-
-xdata_ma = []
-ydata_ma = []
 
 bank = {
     'cash' : 100000,
@@ -26,22 +19,18 @@ i = int(input('i = '))
 while True:
     with open('db.json', 'r+') as f:
         db = json.load(f)
-    xdata = db['xdata']
     ydata = db['ydata']
-    xdata_ma = db['xdata_ma']
     ydata_ma = db['ydata_ma']
         
     priceSell = client.get_sell_price(currency=currency_code)
     priceSpot = client.get_sell_price(currency=currency_code)
     print(float(priceSpot.amount))
 
-    xdata.append(datetime.datetime.now())
     ydata.append(float(priceSell.amount))
             
-    if len(xdata) > i:
+    if len(ydata) > i:
         list_ma = ydata[len(ydata)-i1:len(ydata)-1]
         ydata_ma.append(float(sum(list_ma)) / max(len(list_ma), 1))
-        xdata_ma.append(xdata[len(xdata)-1])                
         if ydata[len(ydata)-1] > ydata_ma[len(ydata_ma)-1] and bank['status'] == False:    
             bank['status'] = True
             bank['coin'] = bank['cash'] / float(priceSpot.amount)
