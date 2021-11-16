4ecc95ff8025d4cf2ec6d8c8e792def409915290
Tim Tim <timrin20@gmail.com>
2021-11-16 19:56:07 +0300

    ok

--- cross.py
+++ cross.py
@@ -1,22 +1,23 @@
-import json
-import datetime
-import time
-import numpy as np
-from coinbase.wallet.client import Client
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from bs4 import BeautifulSoup
 from user_agent import generate_user_agent
+from coinbase.wallet.client import Client
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from bs4 import BeautifulSoup
+import time
 
 client = Client('Su2KNCgW9bZIw9Pp', 'XxTyWtwPou05c52MWc2L7sdu9GRoIlLG', api_version='YYYY-MM-DD')
 currency_code = 'USD'
 
-xdata = []
 ydata = []
 
-xdata_ma = []
 ydata_ma = []
+
+ydata_ma1 = []
 
 bank = {
     'cash' : 100000,
@@ -25,6 +26,8 @@ bank = {
 }
 
 i = int(input('i = '))
+i1 = int(input('i1 = '))
+coin = input('coin = ')
 
 while True:
     #with open('db.json', 'r+') as f:
@@ -36,8 +39,9 @@ while True:
     options.add_argument('--headless')
     options.add_argument('--disable-gpu')
     options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36")
-    driver = webdriver.Chrome(options=options)
-    driver.get('https://www.coinbase.com/price/litecoin')
+    ser = Service("G:\!Timur\Desktop\coinbaseApi\chromedriver.exe")
+    driver = webdriver.Chrome(service=ser, options=options)
+    driver.get('https://www.coinbase.com/price/'+coin)
     time.sleep(3)
 
     main_page = driver.page_source
@@ -54,16 +58,22 @@ while True:
     print(float(quote))
 
     ydata.append(float(quote))
-            
-    if len(ydata) > i:
-        list_ma = ydata[len(ydata)-i:len(ydata)-1]
-        ydata_ma.append(float(sum(list_ma)) / max(len(list_ma), 1))
-        if ydata[len(ydata)-1] > ydata_ma[len(ydata_ma)-1] and bank['status'] == False:    
+
+    if i > 1:        
+        if len(ydata) > i:
+            list_ma = ydata[len(ydata)-i:len(ydata)-1]
+            ydata_ma.append(float(sum(list_ma)) / len(list_ma))
+    else:
+        ydata_ma.append(ydata[len(ydata)-1])
+    if len(ydata) > i1:
+        list_ma1 = ydata[len(ydata)-i1:len(ydata)-1]
+        ydata_ma1.append(float(sum(list_ma1)) / len(list_ma1))
+        if ydata_ma[len(ydata_ma)-1] > ydata_ma1[len(ydata_ma1)-1] and bank['status'] == False:
             bank['status'] = True
             bank['coin'] = bank['cash'] / float(priceSpot)
             bank['cash'] = 0
             print('buy' + '||' + str(bank['cash']) + '||' + str(bank['coin']))
-        if ydata[len(ydata)-1] < ydata_ma[len(ydata_ma)-1] and bank['status'] == True:    
+        if ydata_ma[len(ydata_ma)-1] < ydata_ma1[len(ydata_ma1)-1] and bank['status'] == True:
             bank['status'] = False
             bank['cash'] = bank['coin'] * float(priceSpot)
             bank['coin'] = 0
